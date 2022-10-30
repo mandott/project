2d1b5551a3618b2f68c7941930ff04962019b90c
mandott <mandoteta@gmail.com>
2022-10-30 22:22:41 +0200

    oxi teleytaio commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import {useState, useEffect} from "react";
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
-
+import {TextField} from '@adobe/react-spectrum';
+import {Flex} from 'react-flex';
 /*
  - create a function that sends delete request to server
  - call this function upon click of delete button of each comment
@@ -14,30 +15,48 @@ import 'reactjs-popup/dist/index.css';
 function Comment(props) {
     const handleDelete = () => {
         props.deleteFunc(props.id)
+        closeModal()
     }
-    const [buttonPopup,setButtonPopup] = useState(false)
-    const Modal = () => (
-        <Popup trigger={buttonPopup} modal>
+    const [open, setOpen] = useState(false);
+    const closeModal = () => setOpen(false);
+    const DeleteModal = () => (
+        <Popup open={open} closeOnDocumentClick onClose={closeModal}>
             <span> Are you sure you want to delete this comment?</span>
             <br />
             <button onClick={handleDelete}> Yes  </button>
-                    <button onClick={() => setButtonPopup(false)}>  No</button>
+            <button onClick={closeModal}>  No</button>
         </Popup>
     )
-
+    let [text, setText] = useState();
+    const [secondaryopen, setSecondaryopen] = useState(false);
+    const closeSecondaryModal = () => setSecondaryopen(false);
+    const EditModal = () => (
+        <Popup open={secondaryopen} closeOnDocumentClick onClose={closeSecondaryModal}>
+            <span> How would you like to edit this comment?</span>
+            <br />
+                <TextField
+                    onChange={setText}
+                    label="Your text" />
+                <pre>Mirrored text: {text}</pre>
+            <br />
+            <button onClick={handleDelete}> SAVE  </button>
+            <button onClick={closeSecondaryModal}>  CANCEL</button>
+        </Popup>
+    )
+    //const [value, setValue] = React.useState('Text');
     return (
         <div className="wrapper">
             <div className="container">
                 <div className="email">{props.email}</div>
                 <div className="comment">{props.comment}</div>
-                <div className="deleteButton"><span className="bi bi-trash" onClick={() => setButtonPopup(true)}>Delete</span></div>
-                    <Modal />
-                <div className="editButton"><span className="bi bi-capsule-pill">SwallowMe</span></div>
+                <div className="deleteButton"><span className="bi bi-trash" onClick={() => setOpen(true)}>Delete</span></div>
+                    <DeleteModal />
+                <div className="editButton"><span className="bi bi-capsule-pill" onClick={() => setSecondaryopen(true)}>SwallowMe</span></div>
+                    <EditModal />
             </div>
         </div>
     )
 }
-
 function App() {
     const [comments, setComments] = useState([])
     const [, setLoading] = useState(true);
@@ -87,6 +106,7 @@ function App() {
     return (
         <header>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"/>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
             <div className="grid-container">
                 {listItems}
             </div>
